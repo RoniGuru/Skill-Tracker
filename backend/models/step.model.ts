56bbfcf2ac6192a44301829a7e4178984cbe4682
Ronit Gurung <lecrepes45@gmail.com>
2024-07-04 07:05:06 +0100

    Revert "backend basic"
    This reverts commit b78b3729d763d2d4bd7fdeb267bc210b6fc286af.

--- backend/models/step.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from 'mongoose';
-
-export const StepSchema = new Schema({
-  time: { type: Number, required: [true, 'Please enter time'] },
-
-  date: { type: Date, default: Date.now, required: true },
-});
-
-export const Step = model('Step', StepSchema);
